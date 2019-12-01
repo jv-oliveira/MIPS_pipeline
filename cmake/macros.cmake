7004884d528448fdcb7c78e7547854874657a31f
João Victor Marques de Oliveira <joao.marques.oliveira@usp.br>
2019-12-01 08:35:13 -0300

    make memfile be variable between tests

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -1,16 +1,3 @@
-# Copyright 2017 Pedro Cuadra <pjcuadra@gmail.com>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 set(GHDL_FLAGS --std=08 --ieee=synopsys -fcolor-diagnostics)
 
 # Add sources macro
@@ -40,26 +27,4 @@ macro (add_sources_directory)
       # propagate SRCS to parent directory
       set (VHDL_SOURCES ${VHDL_SOURCES} PARENT_SCOPE)
   endforeach()
-endmacro()
-
-# Add sources macro
-macro (add_mem_init_file)
-    file (RELATIVE_PATH _relPath "${PROJECT_SOURCE_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}")
-    foreach (_src ${ARGN})
-        if (_relPath)
-            list (APPEND MIF_SOURCES "${CMAKE_SOURCE_DIR}/${_relPath}/${_src}")
-            message("-- Adding MIF Source: ${CMAKE_SOURCE_DIR}/${_relPath}/${_src}")
-        else()
-            list (APPEND MIF_SOURCES "${CMAKE_SOURCE_DIR}/${_src}")
-            message("-- Adding MIF Source: ${CMAKE_SOURCE_DIR}/${_src}")
-        endif()
-
-        message("-- Copying ${_src} to ${CMAKE_BINARY_DIR}")
-        file(COPY ${_src} DESTINATION ${CMAKE_BINARY_DIR})
-    endforeach()
-
-    if (_relPath)
-        # propagate SRCS to parent directory
-        set (MIF_SOURCES ${MIF_SOURCES} PARENT_SCOPE)
-    endif()
 endmacro()
--- cmake/test_macros.cmake
+++ cmake/test_macros.cmake
@@ -1,16 +1,3 @@
-# Copyright 2017 Pedro Cuadra <pjcuadra@gmail.com>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 set(GHDL_FLAGS --std=08 --ieee=synopsys --warn-default-binding --warn-binding --warn-specs --warn-delayed-checks --warn-unused)
 
 # Add test sources macro
@@ -33,10 +20,8 @@ macro (add_test_sources)
 
         add_custom_target("${TEST_NAME}" COMMAND ghdl -m -g ${GHDL_FLAGS} --workdir=${CMAKE_BINARY_DIR} ${ENTITY_NAME} DEPENDS index )
         list (APPEND VHDL_SOURCES "${CMAKE_SOURCE_DIR}/${FILE_SRC}")
-        add_custom_target("${TEST_NAME}_cp_mif_files" COMMAND sh -c "ln -s ${CMAKE_BINARY_DIR}/*.mif ${CMAKE_CURRENT_BINARY_DIR}" || true)
-        add_custom_target("${TEST_NAME}_cp_dat_files" COMMAND sh -c "ln -s ${CMAKE_BINARY_DIR}/*.dat ${CMAKE_CURRENT_BINARY_DIR}" || true)
+        add_custom_target("${TEST_NAME}_cp_dat_files" COMMAND sh -c "ln -s ${CMAKE_BINARY_DIR}/${ENTITY_NAME}.dat ${CMAKE_CURRENT_BINARY_DIR}/memfile.dat" || true)
         add_test(NAME "${TEST_NAME}" COMMAND ghdl -r ${GHDL_FLAGS} -g --workdir=${CMAKE_BINARY_DIR} ${ENTITY_NAME} --vcd=${TRACE_PATH} --stop-time=10us)
-        add_dependencies("${TEST_NAME}" "${TEST_NAME}_cp_mif_files")
         add_dependencies("${TEST_NAME}" "${TEST_NAME}_cp_dat_files")
         add_dependencies(check "${TEST_NAME}")
 
@@ -56,4 +41,26 @@ macro (add_test_sources_directory)
       # propagate SRCS to parent directory
       set (VHDL_SOURCES ${VHDL_SOURCES} PARENT_SCOPE)
   endforeach()
+endmacro()
+
+# Add sources macro
+macro (add_mem_init_file)
+    file (RELATIVE_PATH _relPath "${PROJECT_SOURCE_DIR}" "${CMAKE_CURRENT_SOURCE_DIR}")
+    foreach (_src ${ARGN})
+        if (_relPath)
+            list (APPEND MIF_SOURCES "${CMAKE_SOURCE_DIR}/${_relPath}/${_src}")
+            message("-- Adding MIF Source: ${CMAKE_SOURCE_DIR}/${_relPath}/${_src}")
+        else()
+            list (APPEND MIF_SOURCES "${CMAKE_SOURCE_DIR}/${_src}")
+            message("-- Adding MIF Source: ${CMAKE_SOURCE_DIR}/${_src}")
+        endif()
+
+        message("-- Copying ${_src} to ${CMAKE_BINARY_DIR}")
+        file(COPY ${_src} DESTINATION ${CMAKE_BINARY_DIR})
+    endforeach()
+
+    if (_relPath)
+        # propagate SRCS to parent directory
+        set (MIF_SOURCES ${MIF_SOURCES} PARENT_SCOPE)
+    endif()
 endmacro()
